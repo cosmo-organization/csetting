3a903dca4e1d4eb0222a82f9eb81e8193821e4e3
Sonu Aryan <sonuaryan76448002@gmail.com>
2022-09-13 12:24:38 +0530

    printing bytes to output

--- test.cc
+++ test.cc
@@ -1,9 +1,13 @@
 #include "setting.h"
-#include <stdio.h>
+#include <iostream>
 
 int main(const int argc,const char** argv){
 	char buff[4];
 	int data=10;
 	int_to_bytes(data,buff);
+	std::cout<<(short)buff[0]<<std::endl;
+	std::cout<<(short)buff[1]<<std::endl;
+	std::cout<<(short)buff[2]<<std::endl;
+	std::cout<<(short)buff[3]<<std::endl;
 	return 0;
 }
