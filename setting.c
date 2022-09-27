c7ace64b426974a71a6db247b407e729286ad1d6
Sonu Aryan <sonuaryan76448002@gmail.com>
2022-09-27 20:22:00 +0530

    Status Code

--- setting.c
+++ setting.c
@@ -6,67 +6,47 @@
 #include "setting.h"
 
 
-SETTING_API void copy_mem(void* dest,const void* src,size_t start,size_t len){
+
+SETTING_API Status copy_mem(void* dest,const void* src,size_t start,size_t len){
+	
+	if (len<start)return Status_FAIL;
+	
 	char* byte_buff_dest=(char*)dest;
 	const char* byte_buff_src=(const char*)src;
 	int i=0;
 	for (i;i<(start+len);i++){
 		byte_buff_dest[i]=byte_buff_src[i];
 	}
+	
+	return Status_OK;
 }
 
-SETTING_API void is_little(char* pis_little){
+SETTING_API Status is_little(char* pis_little){
 	int check_byte=0x00000001;
 	(*pis_little)=((char*)((void*)&check_byte))[0]&0x01;
+	return Status_OK;
 }
 
-SETTING_API void int_to_bytes(const int __in_integer,char __out_pchar[4]){
+SETTING_API Status int_to_bytes(const int __in_integer,char __out_pchar[4]){
 	char little;
 	is_little(&little);
 	const char* buff=(const char*)((const void*)&__in_integer);
-	/*
-	if (little){
-		__out_pchar[0]=buff[3];
-		__out_pchar[1]=buff[2];
-		__out_pchar[2]=buff[1];
-		__out_pchar[3]=buff[0];
-		return;
-	}
 	
-	__out_pchar[3]=buff[3];
-	__out_pchar[2]=buff[2];
-	__out_pchar[1]=buff[1];
-	__out_pchar[0]=buff[0];
-	*/
-	
-	//Reduced code branch less code
-	
-	__out_pchar[3-3*little]=buff[3];
-	__out_pchar[2-little]=buff[2];
-	__out_pchar[1+little]=buff[1];
-	__out_pchar[3*little]=buff[0];
-	
+	__out_pchar[3-3*(__BYTE_ORDER&__LITTLE_ENDIAN)]=buff[3];
+	__out_pchar[2-(__BYTE_ORDER&__LITTLE_ENDIAN)]=buff[2];
+	__out_pchar[1+ (__BYTE_ORDER & __LITTLE_ENDIAN)]=buff[1];
+	__out_pchar[3* (__BYTE_ORDER & __LITTLE_ENDIAN)]=buff[0];
+	return Status_OK;
 }
-SETTING_API void short_to_bytes(const short __in_short,char __out_pchar[2]){
-	char little;
+SETTING_API Status short_to_bytes(const short __in_short,char __out_pchar[2]){
 	const char* buff=(const char*)((const void*)&__in_short);
-	is_little(&little);
-	/*
-	if (little){
-		__out_pchar[0]=buff[1];
-		__out_pchar[1]=buff[0];
-		return;
-	}
-	__out_pchar[1]=buff[1];
-	__out_pchar[0]=buff[0];*/
 	
-	//Reduced code branch less code
-	
-	__out_pchar[1-little]=buff[1];
-	__out_pchar[1*little]=buff[0];
+	__out_pchar[1- (__BYTE_ORDER & __LITTLE_ENDIAN)]=buff[1];
+	__out_pchar[1* (__BYTE_ORDER & __LITTLE_ENDIAN)]=buff[0];
+	return Status_OK;
 }
 
-SETTING_API void double_to_bytes(const double __in_double,char __out_pchar[8]){
+SETTING_API Status double_to_bytes(const double __in_double,char __out_pchar[8]){
 	union{
 		double double_val;
 		char byte_array[8];
@@ -74,10 +54,7 @@ SETTING_API void double_to_bytes(const double __in_double,char __out_pchar[8]){
 	
 	union_double_char_array_size_8.double_val=__in_double;
 	
-	char little;
-	is_little(&little);
-	
-	if (little){
+#if (__BYTE_ORDER == __LITTLE_ENDIAN)
 		__out_pchar[0]=union_double_char_array_size_8.byte_array[7];
 		__out_pchar[1]=union_double_char_array_size_8.byte_array[6];
 		__out_pchar[2]=union_double_char_array_size_8.byte_array[5];
@@ -87,8 +64,7 @@ SETTING_API void double_to_bytes(const double __in_double,char __out_pchar[8]){
 		__out_pchar[6]=union_double_char_array_size_8.byte_array[1];
 		__out_pchar[7]=union_double_char_array_size_8.byte_array[0];	
 		return;
-	}
-	
+#else
 	__out_pchar[7]=union_double_char_array_size_8.byte_array[7];
 	__out_pchar[6]=union_double_char_array_size_8.byte_array[6];
 	__out_pchar[5]=union_double_char_array_size_8.byte_array[5];
@@ -97,10 +73,12 @@ SETTING_API void double_to_bytes(const double __in_double,char __out_pchar[8]){
 	__out_pchar[2]=union_double_char_array_size_8.byte_array[2];
 	__out_pchar[1]=union_double_char_array_size_8.byte_array[1];
 	__out_pchar[0]=union_double_char_array_size_8.byte_array[0];
-	
+#endif
 	//Reduced or branchless code may be latter
+	
+	return  Status_OK;
 }
-SETTING_API void float_to_bytes(const float __in_float,char __out_pchar[4]){
+SETTING_API Status float_to_bytes(const float __in_float,char __out_pchar[4]){
 	char little;
 	is_little(&little);
 	union{
@@ -124,9 +102,10 @@ SETTING_API void float_to_bytes(const float __in_float,char __out_pchar[4]){
 	__out_pchar[0]=union_float_char_array_size_4.byte_array[0];
 	
 	//Reduced or branchless code may be latter
+	return Status_OK;
 }
 
-SETTING_API void long_long_to_bytes(const long long __in_long_long,char __out_pchar[8]){
+SETTING_API Status long_long_to_bytes(const long long __in_long_long,char __out_pchar[8]){
 	union{
 		long long long_long_val;
 		char byte_array[8];
@@ -159,35 +138,46 @@ SETTING_API void long_long_to_bytes(const long long __in_long_long,char __out_pc
 	__out_pchar[0]=union_long_long_char_array_size_8.byte_array[0];
 	
 	//Reduced or branchless code may be latter
+	
+	return Status_OK;
 }
 
 
-SETTING_API void bytes_to_int(const char __in_pchar[4],int* __out_pint){
+SETTING_API Status bytes_to_int(const char __in_pchar[4],int* __out_pint){
 	
+	return Status_OK;
 }
-SETTING_API void bytes_to_short(const char __in_pchar[2],short* __out_pshort){
+SETTING_API Status bytes_to_short(const char __in_pchar[2],short* __out_pshort){
 	
+	return Status_OK;
 }
-SETTING_API void bytes_to_double(const char __in_pchar[8],double* __out_pdouble){
+SETTING_API Status bytes_to_double(const char __in_pchar[8],double* __out_pdouble){
 	
+	
+	return Status_OK;
 }
-SETTING_API void bytes_to_float(const char __in_pchar[4],float* __out_pfloat){
+SETTING_API Status bytes_to_float(const char __in_pchar[4],float* __out_pfloat){
 	
+	
+	return Status_OK;
 }
-SETTING_API void bytes_to_long_long(const char __in_pchar[8],long long* __out_plong_long){
+SETTING_API Status bytes_to_long_long(const char __in_pchar[8],long long* __out_plong_long){
 	
+	
+	return Status_OK;
 }
 
 
-SETTING_API void create_ktvdp(lpktvdp __out_ktvdp){
+SETTING_API Status create_ktvdp(lpktvdp __out_ktvdp){
 	(*__out_ktvdp)=(pktvdp)malloc(sizeof(ktvdp));
 	(*__out_ktvdp)->key=NULL;
 	(*__out_ktvdp)->field_type=FT_NULL;
 	(*__out_ktvdp)->description=NULL;
+	return Status_OK;
 }
 
 
-SETTING_API void fill_ktvdp(pktvdp __in_ktvdp,short __in_type,const void* __in_value,const char* __in_description){
+SETTING_API Status fill_ktvdp(pktvdp __in_ktvdp,short __in_type,const void* __in_value,const char* __in_description){
 	const char* mem_buffer=(const char*)__in_value;
 	char little;
 	is_little(&little);
@@ -207,5 +197,5 @@ SETTING_API void fill_ktvdp(pktvdp __in_ktvdp,short __in_type,const void* __in_v
 	size_t description_byte_len=strlen(__in_description)+1;
 	__in_ktvdp->description=(char*)malloc(sizeof(char)*description_byte_len);
 	copy_mem(__in_ktvdp->description,__in_description,0,description_byte_len);
-	
+	return Status_OK;
 }
--- setting.h
+++ setting.h
@@ -19,44 +19,67 @@
 #define FT_EMPTY 6
 #define FT_BOOLEAN 7
 
-#if defined(_MSC_VER)
-    #define EXPORT __declspec(dllexport)
-    #define IMPORT __declspec(dllimport)
-#elif defined(__GNUC__)
-    #define EXPORT __attribute__((visibility("default")))
-    #define IMPORT
+#if _MSC_VER
+#	define __BYTE_ORDER 1
+#	define __LITTLE_ENDIAN 1
+#	define __BIG_ENDIAN 0
 #else
-    #define EXPORT
-    #define IMPORT
-    #pragma warning Unknown dynamic link import/export semantics.
+#pragma warning Jack
+#endif
+
+
+#if defined(_MSC_VER) || defined(WIN32)
+#define EXPORT __declspec(dllexport)
+#define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+#define EXPORT __attribute__((visibility("default")))
+#define IMPORT
+#else
+#define EXPORT
+#define IMPORT
+#pragma warning Unknown dynamic link import/export semantics.
 #endif
 
 
 #ifdef SETTING_API_BUILD_MODE
-	#ifdef WIN32
-		#define SETTING_API EXPORT
-	#else
-		#define SETTING_API	EXPORT
-#endif
+#define SETTING_API EXPORT
 #else
-	#ifdef WIN32
-		#define SETTING_API IMPORT
-	#else
-		#define SETTING_API	IMPORT
-	#endif
+#define SETTING_API IMPORT
 #endif
 
 
 #ifdef __cplusplus
-	#define EXTERN_C extern "C"
+#define EXTERN_C extern "C"
 #else
-	#define EXTERN_C
+#define EXTERN_C
 #endif
 
-typedef struct __KTVDP{ //key, type, value and description pair
+typedef unsigned char Status;
+#define Status_OK	1
+#define Status_FAIL	0
+
+#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN || \
+    defined(__BIG_ENDIAN__) || \
+    defined(__ARMEB__) || \
+    defined(__THUMBEB__) || \
+    defined(__AARCH64EB__) || \
+    defined(_MIBSEB) || defined(__MIBSEB) || defined(__MIBSEB__)
+#define _BE_ 1
+#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN || \
+    defined(__LITTLE_ENDIAN__) || \
+    defined(__ARMEL__) || \
+    defined(__THUMBEL__) || \
+    defined(__AARCH64EL__) || \
+    defined(_MIPSEL) || defined(__MIPSEL) || defined(__MIPSEL__)
+#define _LE_ 1
+#else
+#error "I don't know what architecture this is!"
+#endif
+
+typedef struct __KTVDP { //key, type, value and description pair
 	char* key;
 	unsigned short field_type;
-	union{
+	union {
 		char* string_val;
 		int int_val;
 		short short_val;
@@ -66,41 +89,41 @@ typedef struct __KTVDP{ //key, type, value and description pair
 		float float_val;
 	}value;
 	char* description;
-}ktvdp,*pktvdp,**lpktvdp;
+}ktvdp, * pktvdp, ** lpktvdp;
 
-typedef struct __SETTING_PROFILE{
+typedef struct __SETTING_PROFILE {
 	char* profile_name; //Reserved profile_name length to 256
 	unsigned int n_rows;
 	pktvdp field_value_table;
-}setting_profile,*psetting_profile,**lpsetting_profile;
+}setting_profile, * psetting_profile, ** lpsetting_profile;
 
-typedef struct __SETTING{
+typedef struct __SETTING {
 	unsigned long long magic;
 	unsigned char maj_ver;
 	unsigned char min_ver;
 	unsigned int n_profile_table;
 	psetting_profile setting_profile_table;
-}setting,*psetting,**lpsetting;
+}setting, * psetting, ** lpsetting;
 
 
-EXTERN_C SETTING_API void copy_mem(void* dest,const void* src,size_t start,size_t len);
-EXTERN_C SETTING_API void is_little(char* pis_little);
+EXTERN_C SETTING_API Status copy_mem(void* dest, const void* src, size_t start, size_t len);
+EXTERN_C SETTING_API Status is_little(char* pis_little);
 
 //All take any type of byte order primitive but give out only big endian byte order in __out_pchar
-EXTERN_C SETTING_API void int_to_bytes(const int __in_integer,char __out_pchar[4]);
-EXTERN_C SETTING_API void short_to_bytes(const short __in_short,char __out_pchar[2]);
-EXTERN_C SETTING_API void double_to_bytes(const double __in_double,char __out_pchar[8]);
-EXTERN_C SETTING_API void float_to_bytes(const float __in_float,char __out_pchar[4]);
-EXTERN_C SETTING_API void long_long_to_bytes(const long long __in_long_long,char __out_pchar[8]);
+EXTERN_C SETTING_API Status int_to_bytes(const int __in_integer, char __out_pchar[4]);
+EXTERN_C SETTING_API Status short_to_bytes(const short __in_short, char __out_pchar[2]);
+EXTERN_C SETTING_API Status double_to_bytes(const double __in_double, char __out_pchar[8]);
+EXTERN_C SETTING_API Status float_to_bytes(const float __in_float, char __out_pchar[4]);
+EXTERN_C SETTING_API Status long_long_to_bytes(const long long __in_long_long, char __out_pchar[8]);
 
 //Take big endian order byte in __in_pchar and give out machine specific byte ordered primitive
-EXTERN_C SETTING_API void bytes_to_int(const char __in_pchar[4],int* __out_pint);
-EXTERN_C SETTING_API void bytes_to_short(const char __in_pchar[2],short* __out_pshort);
-EXTERN_C SETTING_API void bytes_to_double(const char __in_pchar[8],double* __out_pdouble);
-EXTERN_C SETTING_API void bytes_to_float(const char __in_pchar[4],float* __out_pfloat);
-EXTERN_C SETTING_API void bytes_to_long_long(const char __in_pchar[8],long long* __out_plong_long);
- 
-EXTERN_C SETTING_API void create_ktvdp(lpktvdp __out_ktvdp);
-EXTERN_C SETTING_API void fill_ktvdp(pktvdp __in_ktvdp,short __in_type,const void* __in_value,const char* __in_description);
+EXTERN_C SETTING_API Status bytes_to_int(const char __in_pchar[4], int* __out_pint);
+EXTERN_C SETTING_API Status bytes_to_short(const char __in_pchar[2], short* __out_pshort);
+EXTERN_C SETTING_API Status bytes_to_double(const char __in_pchar[8], double* __out_pdouble);
+EXTERN_C SETTING_API Status bytes_to_float(const char __in_pchar[4], float* __out_pfloat);
+EXTERN_C SETTING_API Status bytes_to_long_long(const char __in_pchar[8], long long* __out_plong_long);
+
+EXTERN_C SETTING_API Status create_ktvdp(lpktvdp __out_ktvdp);
+EXTERN_C SETTING_API Status fill_ktvdp(pktvdp __in_ktvdp, short __in_type, const void* __in_value, const char* __in_description);
 
 #endif
